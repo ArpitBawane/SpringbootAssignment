b2f5efb67645469d375907462bf1185a4e6f29d2
ArpitBawane <arpit.bawane@mindcraft.in>
2026-02-04 18:30:50 +0530

    javaassignupload

---
+++ src/main/java/com/mindcraft/responseDTO/ApiResponse.java
@@ -0,0 +1,40 @@
+package com.mindcraft.responseDTO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+public class ApiResponse<T> {
+
+    public ApiResponse(String status, String message, T data) {
+		super();
+		this.status = status;
+		this.message = message;
+		this.data = data;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	private String status;
+    private String message;
+    private T data;
+    
+
+}
